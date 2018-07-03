a7c81e56966a0a7a67cd07fc9b350e9f0cad91e1
/ Holly <40799309+OliveiraPinheiro@users.noreply.github.com>
2018-07-03 16:49:05 +0100

    Update peso.py

--- peso.py
+++ peso.py
@@ -29,16 +29,16 @@ if op == "A":
         continue
        
 
-elif op == "B":
-    print(""" TABELA DE MEDIDA DO COPO
-Peso        Medida
-50 a 55  kg 150ml
-55 a 65  kg 200ml
-65 a 75  kg 250ml
-75 a 100 kg 300ml
-90 a 100 kg 305ml
-acima    kg  400mL """)
-elif op == "C":
-    sys.exit()
+          elif op == "B":
+           print(""" TABELA DE MEDIDA DO COPO
+          Peso        Medida
+          50 a 55  kg 150ml
+          55 a 65  kg 200ml
+          65 a 75  kg 250ml
+          75 a 100 kg 300ml
+          90 a 100 kg 305ml
+          acima    kg  400mL """)
+          elif op == "C":
+                sys.exit()
 else:
-    print("")
+    print("ByeBye")
